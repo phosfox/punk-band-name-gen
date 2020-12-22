84519efb3844b119f2696ec6337e3d74d52c208a
phosfox <mordredur@googlemail.com>
2020-12-22 17:26:09 +0100

    init

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Anarchy" src="./assets/anarchy.png" width="300" height="300">
+    <Home></Home>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Home from './components/Home.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Home
   }
 }
 </script>
---
+++ src/components/Home.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <div>
+      <p>The {{ bandName }}</p>
+    </div>
+    <div>
+      <button v-on:click="newBandName">New Bandname</button>
+    </div>
+  </div>
+</template>
+
+<script>
+import { words } from "../assets/words.js";
+
+String.prototype.capitalize = function () {
+  return this.charAt(0).toUpperCase() + this.slice(1);
+};
+const sample = (arr) => {
+  return arr[Math.floor(Math.random() * arr.length)];
+};
+export default {
+  data: () => {
+    return {
+      bandName: "Placeholders",
+      bandNames: [],
+    };
+  },
+  created() {
+    this.bandNames = words;
+  },
+  methods: {
+    newBandName: function () {
+      this.bandName = sample(this.bandNames).capitalize();
+    },
+  },
+};
+</script>
+
+<style>
+</style>
